d8c3562f500ac8bbd7fdb5fde14d169a768d8daa
Hugo Ribeira <hugoribeira@gmail.com>
2016-10-19 18:57:50 +0100

    Chapter 3 - Multiple Bucket Registries
    This commit uses multiple Bucket Registries as to allow concurrent requests to
    performed at the same time against buckets that live in different registries.
    
    This removes the bottleneck of having all requests pass through the same
    BucketRegistry as in Elixir, a process can only handle one message at a time.

--- lib/distributed_kv.ex
+++ lib/distributed_kv.ex
@@ -3,13 +3,60 @@ defmodule DistributedKv do
 
   alias DistributedKv.BucketRegistry
 
-  def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+  @number_of_registries Application.get_env(:distributed_kv, :number_of_registries)
 
-    children = [
-      worker(BucketRegistry, [[name: BucketRegistry]])
-    ]
+  def start(_type, _args) do
+    children = Enum.map(1..4, &bucket_worker_spec/1)
     opts = [strategy: :one_for_one, name: DistributedKv.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp bucket_worker_spec(index) do
+    import Supervisor.Spec, warn: false
+    name = registry_name(index)
+    worker(BucketRegistry, [[name: name]], id: name)
+  end
+
+  @doc """
+  Returns the pid of the registry where a given name might reside.
+
+  This is used to distribute load for multiple registries so that
+  a single registry doesn't become a bottleneck.
+
+  To known whether a given bucket name exists or not, the BucketRegistry
+  functions should be called, as registry_for/1 gives no guarantees as
+  to whether that bucket exists or not. The only guarantee is that if
+  it exists, it will reside in the returned BucketRegistry.
+  """
+  def registry_for(name) do
+    registry = name
+    |> registry_number
+    |> registry_name
+    |> Process.whereis
+
+    case registry do
+      nil ->
+        :timer.sleep(10)
+        registry_for(name)
+      pid -> pid
+    end
+  end
+
+  defp registry_number(name) do
+    :erlang.phash2(name, @number_of_registries) + 1
+  end
+
+  defp registry_name(index) when is_number(index) do
+    :"BucketRegistry#{index}"
+  end
+
+  @doc """
+  Returns all alive registries
+  """
+  def registries do
+    1..4
+    |> Enum.map(&registry_name/1)
+    |> Enum.map(&Process.whereis/1)
+    |> Enum.reject(&(&1 == nil))
   end
 end
--- test/distributed_kv_test.exs
+++ test/distributed_kv_test.exs
@@ -1,3 +1,37 @@
 defmodule DistributedKvTest do
   use ExUnit.Case
+
+  describe "registry_for/1" do
+    test "returns different registries for different names" do
+      shopping_registry = DistributedKv.registry_for("Shopping list")
+      another_registry = DistributedKv.registry_for("Another")
+      assert shopping_registry != another_registry
+    end
+
+    test "returns the same registry if the same name is given" do
+      shopping_registry = DistributedKv.registry_for("Shopping list")
+      another_registry = DistributedKv.registry_for("Shopping list")
+      assert shopping_registry == another_registry
+    end
+  end
+
+  describe "registries/0" do
+    test "returns all registries" do
+      registries = DistributedKv.registries
+      assert Enum.count(registries) == Application.get_env(:distributed_kv, :number_of_registries)
+    end
+  end
+
+  test "does not touch other registries if one crashes" do
+    registry = DistributedKv.registry_for("one")
+    another_registry = DistributedKv.registry_for("four")
+    assert registry != another_registry
+
+    Process.exit(registry, :kill)
+    assert Process.alive?(registry) == false
+
+    new_registry_for_one = DistributedKv.registry_for("one")
+    assert Process.alive?(new_registry_for_one) == true
+    assert registry != new_registry_for_one
+  end
 end
